1d5709ca8f97efd6f15cd8f467834d78ba42f647
Falcon Dai <dai@uchicago.edu>
2013-09-23 16:19:52 -0500

    example changed to use general RANSAC framework

--- line_fitting.py
+++ line_fitting.py
@@ -1,54 +1,32 @@
 import numpy as np
+from ransac import *
 
 def augment(xys):
 	axy = np.ones((len(xys), 3))
 	axy[:, :2] = xys
 	return axy
 
-def sample(xys, n):
-	np.random.shuffle(xys)
-	return xys[:n]
-
 def estimate(xys):
-	axy = augment(xys)
+	axy = augment(xys[:2])
 	return np.linalg.svd(axy)[-1][-1, :]
 
-def count_inliers(xys, coeffs, threshold):
-	return len(filter(lambda e: np.abs(e) < threshold, coeffs.dot(augment(xys).T)))
-
-
+def is_inlier(coeffs, xy, threshold):
+	return np.abs(coeffs.dot(augment([xy]).T)) < threshold
 
 if __name__ == '__main__':
 	from matplotlib import pylab
-
+        
 	n = 100
 	max_iterations = 100
 	goal_inliers = n * 0.3
-
+        
 	# test data
 	xys = np.random.random((n, 2)) * 10
 	xys[:50, 1:] = xys[:50, :1]
-
+        
 	pylab.scatter(xys.T[0], xys.T[1])
 
 	# RANSAC
-	best = 0
-	best_model = None
-	for i in xrange(max_iterations):
-		s = sample(xys, 2)
-		m = estimate(s)
-		ic = count_inliers(xys, m, 0.01)
-		
-		print s
-		print 'estimate:', m,
-		print '# inliers:', ic
-		a, b, c = m
-		pylab.plot([0, 10], [-c/b, -(c+10*a)/b])
-		
-		if ic > best:
-			best = ic
-			best_model = m
-			# pylab.scatter(s.T[0], s.T[1], color='red')
-			if ic > goal_inliers:
-				break
-	print 'took iterations:', i+1, 'best model:', best_model, 'explains:', best
+        m, b = run_ransac(xys, estimate, lambda x, y: is_inlier(x, y, 0.01), goal_inliers, max_iterations, 20)
+        a, b, c = m
+	pylab.plot([0, 10], [-c/b, -(c+10*a)/b], color=(0, 1, 0))
--- plane_fitting.py
+++ plane_fitting.py
@@ -5,18 +5,12 @@ def augment(xyzs):
 	axyz[:, :3] = xyzs
 	return axyz
 
-def sample(xyzs, n):
-	np.random.shuffle(xyzs)
-	return xyzs[:n]
-
 def estimate(xyzs):
-	axyz = augment(xyzs)
+	axyz = augment(xyzs[:3])
 	return np.linalg.svd(axyz)[-1][-1, :]
 
-def count_inliers(xyzs, coeffs, threshold):
-	return len(filter(lambda e: np.abs(e) < threshold, coeffs.dot(augment(xyzs).T)))
-
-
+def is_inlier(coeffs, xyz, threshold):
+	return np.abs(coeffs.dot(augment([xyz]).T)) < threshold
 
 if __name__ == '__main__':
 	from matplotlib import pylab
@@ -25,7 +19,7 @@ if __name__ == '__main__':
 	ax = mplot3d.Axes3D(fig)
 
 	def plot_plane(a, b, c, d):
-		xx, yy = np.mgrid[4:6, 4:6]
+		xx, yy = np.mgrid[:10, :10]
 		return xx, yy, (-d - a * xx - b * yy) / c
 
 	n = 100
@@ -39,24 +33,8 @@ if __name__ == '__main__':
 	ax.scatter3D(xyzs.T[0], xyzs.T[1], xyzs.T[2])
 
 	# RANSAC
-	best = 0
-	best_model = None
-	for i in xrange(max_iterations):
-		s = sample(xyzs, 3)
-		m = estimate(s)
-		ic = count_inliers(xyzs, m, 0.01)
+        m, b = run_ransac(xyzs, estimate, lambda x, y: is_inlier(x, y, 0.01), 3, goal_inliers, max_iterations)
+        a, b, c, d = m
+        xx, yy, zz = plot_plane(a, b, c, d)
+        ax.plot_surface(xx, yy, zz, color=(0, 1, 0, 0.5))
 		
-		print s
-		print 'estimate:', m,
-		print '# inliers:', ic
-		a, b, c, d = m
-		xx, yy, zz = plot_plane(a, b, c, d)
-		ax.plot_surface(xx, yy, zz)
-		
-		if ic > best:
-			best = ic
-			best_model = m
-			# ax.scatter3D(s.T[0], s.T[1], s.T[2], color='red')
-			if ic > goal_inliers:
-				break
-	print 'took iterations:', i+1, 'best model:', best_model, 'explains:', best
