9e3b0568ef1b5f7e48049cc3824b25d45c9a26ad
Luca Manzari <manzariluca@gmail.com>
2018-07-05 14:31:44 +0200

    replace pylab with pyplot

--- line_fitting.py
+++ line_fitting.py
@@ -14,7 +14,8 @@ def is_inlier(coeffs, xy, threshold):
     return np.abs(coeffs.dot(augment([xy]).T)) < threshold
 
 if __name__ == '__main__':
-    from matplotlib import pylab
+    import matplotlib
+    import matplotlib.pyplot as plt
 
     n = 100
     max_iterations = 100
@@ -24,9 +25,11 @@ if __name__ == '__main__':
     xys = np.random.random((n, 2)) * 10
     xys[:50, 1:] = xys[:50, :1]
 
-    pylab.scatter(xys.T[0], xys.T[1])
+    plt.scatter(xys.T[0], xys.T[1])
 
     # RANSAC
     m, b = run_ransac(xys, estimate, lambda x, y: is_inlier(x, y, 0.01), goal_inliers, max_iterations, 20)
     a, b, c = m
-    pylab.plot([0, 10], [-c/b, -(c+10*a)/b], color=(0, 1, 0))
+    plt.plot([0, 10], [-c/b, -(c+10*a)/b], color=(0, 1, 0))
+
+    plt.show()
